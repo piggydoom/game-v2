c125f842476150d1e92cc34508a70494b2dee317
artist piggy <artistpiggy@gmail.com>
2024-02-05 12:46:04 +1100

    added background and plane

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 let myGameArea = {
     canvas : document.createElement("canvas"),
     start : function(){
-        this.canvas.width=480;
-        this.canvas.width=270;
+        this.canvas.width=100 * window.innerWidth / 100;
+        this.canvas.height=95 * window.innerHeight / 100;
         this.context = this.canvas.getContext("2d");
-        // document.getElementById("gameCanvas").appendChild(this.canvas);
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        document.getElementById("gameCanvas").appendChild(this.canvas);
+        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         // this.interval = setInterval(updateGameArea, 20);
         },
@@ -18,20 +18,21 @@ let myGameArea = {
 }
 
 
+
 function startGame() {
     myGameArea.start();
+    let ctx = myGameArea.context;
 
+    ctx.fillStyle = "#2bceff";
+    ctx.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
+    // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    ctx.drawImage(planeImage, 0, 0, 128, 128, myGameArea.canvas.width / 2, myGameArea.canvas.height / 2, 64, 64);
+    
 }
 
 let planeImage = new Image();
-let ctx = myGameArea.context;
-// console.log(myGameArea.canvas);
-ctx.drawImage(planeImage, 100, 100);
 
 
-planeImage.onload = function(){
-  
-    
-}
+
 
 planeImage.src = "styles/Images/plane.png";
