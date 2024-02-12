56d233b955b8714633e0fe4d98a40dcbc04ea276
artist piggy <artistpiggy@gmail.com>
2024-02-12 12:35:53 +1100

    adding movement wip

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,70 @@
-let myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function(){
-        this.canvas.width=100 * window.innerWidth / 100;
-        this.canvas.height=95 * window.innerHeight / 100;
-        this.context = this.canvas.getContext("2d");
-        document.getElementById("gameCanvas").appendChild(this.canvas);
-        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        // this.interval = setInterval(updateGameArea, 20);
-        },
-//     clear : function() {
-//         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-//     },
-//     stop : function() {
-//         clearInterval(this.interval);
-//     }
-}
-
-
-
-function startGame() {
-    myGameArea.start();
-    let ctx = myGameArea.context;
-
-    ctx.fillStyle = "#2bceff";
-    ctx.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
-    // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-    ctx.drawImage(planeImage, 0, 0, 128, 128, myGameArea.canvas.width / 2, myGameArea.canvas.height / 2, 64, 64);
-    
-}
-
 let planeImage = new Image();
 
 
 
 
 planeImage.src = "styles/Images/plane.png";
+
+let myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function(){
+
+        //sets the canvas width + height
+        this.canvas.width=100 * window.innerWidth / 100;
+        this.canvas.height=90 * window.innerHeight / 100;
+
+        //set canvas contex to 2d 
+        this.context = this.canvas.getContext("2d");
+
+        //gets div element called gameCanvas and adds the canvas
+        document.getElementById("gameCanvas").appendChild(this.canvas);
+        
+        //counts the frames
+        this.frameNo = 0;
+
+        //frame interval counter
+        // this.interval = setInterval(updateGameArea, 20);
+
+        },
+
+    //clearing the screen
+    clear : function() {
+       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+}
+
+
+//waits for page body to load then runs startGame
+function startGame() {
+    myGameArea.start();
+    
+
+    //set background to blue
+    ctx.fillStyle = "#2bceff";
+    ctx.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
+
+
+}
+let pX = 100;
+function animate(){
+
+    // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    ctx.drawImage(planeImage, 0, 0, 128, 128, pX, myGameArea.canvas.height / 2, 64, 64);
+  
+
+    window.requestAnimationFrame(animate);
+};
+
+addEventListener("keydown", (event) => {});
+
+onkeydown = (event) => {
+    // console.log(event.key);
+    
+    if(event.key == "a" || event.key == "A" ){
+    pX += 5;
+    }
+
+};
+
+
+window.requestAnimationFrame(animate);
