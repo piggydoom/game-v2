56bad42ab0e0d031706d1bc6ec9f40b2d1718672
artist piggy <artistpiggy@gmail.com>
2024-03-04 12:53:26 +1100

    wip

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 let playerPlane;
-let planeImage = new Image();
+const planeImage = new Image();
+const cloudImage = new Image();
 let ctx;
+let cloud1;
+
+
 
 
 
@@ -25,13 +29,14 @@ let myGameArea = {
 
         //frame interval counter
         // this.interval = setInterval(updateGameArea, 20);
-        this.context.fillStyle = "#2bceff";
-        this.context.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
+      
     },
 
     //clearing the screen
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = "#2bceff";
+        this.context.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
     },
 }
 
@@ -39,28 +44,48 @@ let myGameArea = {
 //waits for page body to load then runs startGame
 function startGame() {
     myGameArea.start();
-    window.onload = function(){
-        playerPlane = new component(planeImage, 128, 128, myGameArea.canvas.innerWidth / 2, myGameArea.canvas.innerHeight * 0.8);
-        ctx = myGameArea.context;
+    ctx = myGameArea.context;
+    planeImage.onload = function () {
+        playerPlane = new Player(planeImage, //image
+            128, //width 
+            128, //height
+            Math.ceil(myGameArea.canvas.width / 2 - 64), //Xpos
+            Math.ceil(myGameArea.canvas.height * 0.7)); //Ypos
+        window.requestAnimationFrame(animate);
+
+    cloudImage.onload = function () {
+        cloud1 = new Component(cloudImage,
+            32,
+            32,
+            getRandomInt(myGameArea.canvas.width),
+            getRandomInt(myGameArea.canvas.height))
     }
-   
+
+
+        // console.log(Math.ceil(myGameArea.canvas.width / 2));
+        // console.log(myGameArea.canvas.width);
+
+
+    }
+
     planeImage.src = "styles/Images/plane.png";
-  
+
 }
 function animate() {
-    console.log("test");
-   playerPlane.update();
-   
+
+    myGameArea.clear();
+    playerPlane.update();
+    cloud1.update();
 
 
 
 
-  
+
     window.requestAnimationFrame(animate);
 };
 
 
-function component(image, width, height, pX, pY){
+function Player(image, width, height, pX, pY) {
     this.image = image;
     this.width = width;
     this.height = height;
@@ -68,23 +93,62 @@ function component(image, width, height, pX, pY){
     this.pY = pY;
 
 
-    this.update = function(){
+     
+    this.update = function () {
+        //controls
+        addEventListener("keydown", (event) => { });
+
+        onkeydown = (event) => {
+            console.log(event.key);
+            
+            //move left when a is pressed
+            if (event.key == "a" || event.key == "A") {
+                this.pX += -5;
+            }
+
+            //move right when d is pressed
+            else if (event.key == "d" || event.key == "D"){
+                this.pX += 5;
+            }
+            
+            //move up when w is pressed
+            else if (event.key == "w" || event.key == "W"){
+                this.pY += -5;
+            }
+
+            //move down when s is pressed
+
+            else if (event.key == "s" || event.key == "S"){
+                this.pY += 5;
+            }
+        };
+
 
         // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-        ctx.drawImage(this.image, 0, 0, 128, 128, myGameArea.canvas.innerWidth / 2, myGameArea.canvas.innerHeight / 2, 128, 128 )
+        ctx.drawImage(this.image, 0, 0, 128, 128, this.pX, this.pY, 128, 128)
     }
 };
 
-addEventListener("keydown", (event) => { });
 
-onkeydown = (event) => {
-    // console.log(event.key);
 
-    if (event.key == "a" || event.key == "A") {
-        pX += 5;
+function Component(image, width, height, pX, pY) {
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.pX = pX;
+    this.pY = pY;
+
+
+     
+    this.update = function () {
+ 
+
+        // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+        ctx.drawImage(this.image, 0, 0, 32, 32, this.pX, this.pY, 128, 128)
     }
-
 };
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-window.requestAnimationFrame(animate);
