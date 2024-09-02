5e64dc9dca260ce0e1920af99c9fb02856f89d05
Matthew Robertson <mattjr4@hotmail.com>
2024-09-02 12:26:17 +0930

    fixed explosion and health invulnerablity

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,7 @@ let timePassed = 0;
 let clouds = [];
 let f16s = [];
 let su27s = [];
+let explosionImage;
 const loopingCessnaAudio = new Audio("../styles/audio/cessna-looping.wav");
 let f16SpeedMultiplier = [2, 3, 6];
 let speedMultiplierIndex;
@@ -19,10 +20,13 @@ const gameCanvasDiv = document.getElementById("gameCanvas");
 let playerDamageTimer = 0;
 let healthBar = document.getElementById("health-bar");
 let currentHealth = 100;
+let lastCollisionTime = 0;
+const COLLISION_COOLDOWN = 1000;
 
 let myGameArea = {
     canvas: document.createElement("canvas"),
-    start: function () {
+    start: function ()
+    {
 
         //sets the canvas width + height
         this.canvas.width = 100 * window.innerWidth / 100;
@@ -43,14 +47,16 @@ let myGameArea = {
     },
 
     //clearing the screen
-    clear: function () {
+    clear: function ()
+    {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.fillStyle = "#2bceff";
         this.context.fillRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
     },
 }
 
-function gameLoop(timeStamp) {
+function gameLoop(timeStamp)
+{
     //calculate how much time has passed
     secondsPassed = (timeStamp - oldTimeStamp) / 1000;
     oldTimeStamp = timeStamp;
@@ -76,41 +82,48 @@ function gameLoop(timeStamp) {
     window.requestAnimationFrame(gameLoop);
 
     playerPlane.draw();
-    clouds.forEach((cloud) => {
+    clouds.forEach((cloud) =>
+    {
         cloud.draw();
 
 
     });
 
-    f16s.forEach((f16) => {
+    f16s.forEach((f16) =>
+    {
         f16.draw();
     });
 
-    su27s.forEach((su27) => {
+    su27s.forEach((su27) =>
+    {
         su27.draw();
     });
 
 }
 //game logic loop
-function update(secondsPassed) {
+function update(secondsPassed)
+{
 
     timePassed += secondsPassed;
     playerPlane.update(timePassed);
 
 
-    clouds.forEach((cloud) => {
+    clouds.forEach((cloud) =>
+    {
         cloud.update(2);
     });
 
 
     //itterate over each f16 and apply collision
-    f16s.forEach((f16) => {
+    f16s.forEach((f16) =>
+    {
         f16.update(5);
         collision(playerPlane, f16);
     }
     );
 
-    su27s.forEach((su27) => {
+    su27s.forEach((su27) =>
+    {
         su27.update(7);
         collision(playerPlane, su27);
     }
@@ -122,11 +135,13 @@ function update(secondsPassed) {
 
 
 //waits for page body to load then runs startGame
-function startGame() {
+function startGame()
+{
     startScreen.style.animation = "fade-out 3s forwards";
     gameCanvasDiv.style.display = "none";
     myGameArea.start();
-    setInterval(() => {
+    setInterval(() =>
+    {
         startScreen.style.display = "none";
         gameCanvasDiv.style.display = "block";
     }, 3000);
@@ -135,7 +150,11 @@ function startGame() {
 
     loopingCessnaAudio.volume = 0.25;
 
-    loadImages(sources, function (images) {
+    loadImages(sources, function (images)
+    {
+
+        explosionImage = images.explosion;
+
         playerPlane = new Player(images.plane,
             128, //width 
             128, //height
@@ -148,32 +167,35 @@ function startGame() {
 
 
         //spawn f16s
-        setInterval(() => {
-        
+        setInterval(() =>
+        {
+
             let w = 76;
             let h = 128;
             let x = Math.random() * Math.abs(myGameArea.canvas.width - w); //randomize X location to change spawning location
             let y = -100;
             f16s.push(new Entity(images.f16, w, h, x, y, "f16", true));
-        }, getRandomInt(13000) + 3000);
-        
-        
-        
+        }, getRandomInt(7000) + 3000);
+
+
+
         //spawn su27
-        setInterval(() => {
+        setInterval(() =>
+        {
             console.log("test");
             let w = 128;
             let h = 197;
             let x = Math.random() * Math.abs(myGameArea.canvas.width - w); //randomize X location to change spawning location
             let y = -100;
             su27s.push(new Entity(images.su27, w, h, x, y, "su27", true));
-        }, getRandomInt(17000) + 3000);
-        
-        
-        
-        
+        }, getRandomInt(4000) + 3000);
+
+
+
+
         //spawn clouds
-        setInterval(() => {
+        setInterval(() =>
+        {
             let w = 128;
             let h = 128;
             let x = Math.random() * Math.abs(myGameArea.canvas.width - w);
@@ -182,7 +204,8 @@ function startGame() {
             clouds.push(new Entity(images.cloud1, w, h, x, y, "cloud", false));
         }, 300);
 
-        setInterval(() => {
+        setInterval(() =>
+        {
             let w = 128;
             let h = 128;
             let x = Math.random() * Math.abs(myGameArea.canvas.width - w);
@@ -218,22 +241,26 @@ let keyMap = {
 }
 
 
-function keyDown(event) {
+function keyDown(event)
+{
     let key = keyMap[event.keyCode];
     keyPress[key] = true;
     loopingCessnaAudio.autoplay = true;
     loopingCessnaAudio.loop = true;
 };
 
-function keyUp(event) {
+function keyUp(event)
+{
     let key = keyMap[event.keyCode];
     keyPress[key] = false;
     speedMultiplierIndex = 1;
 
 }
 
-function touchHandler(event) {
-    if (event.touches) {
+function touchHandler(event)
+{
+    if (event.touches)
+    {
         mobileTouch.xPos = event.touches[0].pageX - myGameArea.canvas.offsetLeft - 128 / 2;
         mobileTouch.yPos = event.touches[0].pageY - myGameArea.canvas.offsetTop - 128 / 2;
         mobileTouch.isPressed = true;
@@ -246,7 +273,8 @@ window.addEventListener("touchstart", touchHandler);
 window.addEventListener("touchmove", touchHandler);
 
 
-function Player(image, width, height, pX, pY) {
+function Player(image, width, height, pX, pY)
+{
 
     this.image = image;
     this.width = width;
@@ -258,52 +286,65 @@ function Player(image, width, height, pX, pY) {
     this.isDamaged = false;
     this.currentFrame = 0;
     this.recentlyDamaged = false;
+    this.lastDamageTime = 0;
+    this.invulnerabilityPeriod = 1000;
 
-    this.draw = function () {
+    this.draw = function ()
+    {
 
-        if (this.recentlyDamaged) {
+        if (this.recentlyDamaged)
+        {
             playerDamageTimer++;
-            if (playerDamageTimer < 360) {
-                if (myGameArea.frameNo % 10) {
+            if (playerDamageTimer < 360)
+            {
+                if (myGameArea.frameNo % 10)
+                {
                     ctx.drawImage(this.image, this.damageFrames[this.currentFrame] * 128, 0, 128, 128, this.pX, this.pY, this.width, this.height);
                 }
-            } else {
+            } else
+            {
                 this.recentlyDamaged = false;
                 playerDamageTimer = 0;
             }
         }
-        else if (this.isDamaged) {
+        else if (this.isDamaged)
+        {
             // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
             ctx.drawImage(this.image, this.damageFrames[this.currentFrame] * 128, 0, 128, 128, this.pX, this.pY, this.width, this.height);
-        } else {
+        } else
+        {
 
             // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
             ctx.drawImage(this.image, 0, 0, 128, 128, this.pX, this.pY, this.width, this.height);
         }
     }
 
-    this.update = function () {
+    this.update = function ()
+    {
         const distance = this.speed;
         this.pX = Math.max(0, Math.min(myGameArea.canvas.width - this.width, this.pX));
         this.pY = Math.max(0, Math.min(myGameArea.canvas.height - this.height, this.pY));
 
 
 
-        if (keyPress.left) {
+        if (keyPress.left)
+        {
             this.pX -= distance;
             // if (this.pX <= 0) {
             //     this.pX = myGameArea.canvas.width - this.width;
             // }
         }
 
-        if (keyPress.right) {
+        if (keyPress.right)
+        {
             this.pX += distance;
             // if (this.pX >= myGameArea.canvas.width - this.width) {
             //     this.pX = 0;
             // }
         }
 
-        if (keyPress.up) {
+        if (keyPress.up)
+        {
             speedMultiplierIndex = 2;
             this.pY -= distance;
             // if (this.pY <= 0) {
@@ -312,7 +353,8 @@ function Player(image, width, height, pX, pY) {
             // }
         }
 
-        if (keyPress.down) {
+        if (keyPress.down)
+        {
             speedMultiplierIndex = 0;
             this.pY += distance;
             // if (this.pY >= myGameArea.canvas.height - this.height) {
@@ -320,17 +362,21 @@ function Player(image, width, height, pX, pY) {
             // }
         }
 
-        if (mobileTouch.isPressed) {
+        if (mobileTouch.isPressed)
+        {
             speedMultiplierIndex = 1;
             this.pY = mobileTouch.yPos;
             this.pX = mobileTouch.xPos;
         }
 
-        if (this.isDamaged) {
-            if (myGameArea.frameNo % 10 == 0) {
+        if (this.isDamaged)
+        {
+            if (myGameArea.frameNo % 10 == 0)
+            {
                 this.currentFrame++;
             }
-            if (this.currentFrame >= this.damageFrames.length) {
+            if (this.currentFrame >= this.damageFrames.length)
+            {
                 this.currentFrame = 1;
             }
         };
@@ -351,12 +397,31 @@ function Player(image, width, height, pX, pY) {
         //     };
         // }
 
+        this.canTakeDamage = function ()
+        {
+            return Date.now() - this.lastDamageTime > this.invulnerabilityPeriod;
+        }
+
+        this.takeDamage = function ()
+        {
+            if (this.canTakeDamage())
+            {
+                this.isDamaged = true;
+                this.recentlyDamaged = true;
+                this.lastDamageTime = Date.now();
+                currentHealth = Math.max(0, currentHealth - 10); // Ensure health doesn't go below 0
+                healthBar.style.width = currentHealth + "%";
+            }
+        }
+
     }
 }
 
 
-class Entity {
-    constructor(img, width, height, pX, pY, type, collidable) {
+class Entity
+{
+    constructor(img, width, height, pX, pY, type, collidable)
+    {
         this.image = img;
         this.width = width;
         this.height = height;
@@ -365,58 +430,73 @@ class Entity {
         this.isDamaged = false;
         this.collidable = collidable;
         this.explosionFrame = 0;
+        this.explosionFrameCounter = 0;
 
     }
 
 
 
 
-    draw() {
+    draw()
+    {
 
-        if(this.isDamaged){
-            ctx.drawImage(images.explosion, explosionFrame, 0, this.width, this.height, this.pX, this.pY, this.width, this.height)
-            this.explosionFrame++;
+        if (this.isDamaged)
+        {
+            if (this.explosionFrame > 32)
+            {
+                this.isDamaged = false;
+                delete this.pY;
+            }
+
+            this.explosionFrameCounter++;
+
+            if (this.explosionFrameCounter % 400)
+            {
+                this.explosionFrame++;
+            }
+
+            ctx.drawImage(explosionImage, this.explosionFrame * 64, 0, 64, 64, this.pX - 128, this.pY, 384, 384);
+            console.log(this.explosionFrame);
         }
-        // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-        ctx.drawImage(this.image, 0, 0, this.width, this.height, this.pX, this.pY, this.width, this.height)
+        else
+        {
+            // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+            ctx.drawImage(this.image, 0, 0, this.width, this.height, this.pX, this.pY, this.width, this.height);
+        }
     }
 
-    update(speed) {
-        this.pY = this.pY + f16SpeedMultiplier[speedMultiplierIndex] * speed;
+    update(speed)
+    {
 
-    }
-}
+            this.pY = this.pY + f16SpeedMultiplier[speedMultiplierIndex] * speed;
 
-class Explosion{
-    constructor(image, width, height){
-        this.image = image;
-        this.width = width;
-        this.height = height
-
-    }
-    draw(Xpos, Ypos){
-        ctx.drawImage(this.image, 0, 0, this.width, this.height, Xpos, Ypos, this.width, this.height);
     }
 }
 
 
-function getRandomInt(max) {
+function getRandomInt(max)
+{
     return Math.floor(Math.random() * max);
 }
 
 
-function loadImages(sources, callback) {
+function loadImages(sources, callback)
+{
     var images = {};
     var loadedImages = 0;
     var numImages = 0;
     // get num of sources
-    for (var src in sources) {
+    for (var src in sources)
+    {
         numImages++;
     }
-    for (var src in sources) {
+    for (var src in sources)
+    {
         images[src] = new Image();
-        images[src].onload = function () {
-            if (++loadedImages >= numImages) {
+        images[src].onload = function ()
+        {
+            if (++loadedImages >= numImages)
+            {
                 callback(images);
             }
         };
@@ -425,19 +505,23 @@ function loadImages(sources, callback) {
 }
 
 
-function collision(player, object) {
+function collision(player, object)
+{
+
 
     if (
         player.pX + player.width >= object.pX &&  //check if player right hand side touches object left hand side
         object.pX + object.width >= player.pX && //check if object right hand side touches player left hand side
         player.pY + player.height >= object.pY && //check if player bottom side touches object top side
         object.pY + object.height >= player.pY //check if player top side touches object bottom side
-    ) {
-        playerPlane.isDamaged = true;
-        playerPlane.recentlyDamaged = true;
-        currentHealth = currentHealth -10;
-        healthBar.style.width = currentHealth + "%";
-        explosion = new Explosion(object.pX, object.pY);
-        
+    )
+    {
+        const currentTime = Date.now();
+        if (currentTime - lastCollisionTime > COLLISION_COOLDOWN)
+        {
+            player.takeDamage();
+            object.isDamaged = true;
+            lastCollisionTime = currentTime;
+        }
     }
 }
